ee656dba2210b5f16457dfec287d3335c9eb0f43
Mapachana <anabuenrua99@gmail.com>
2020-01-31 23:33:32 +0100

    Corregido pequeño fallo de player y gui

--- proyecto/escenas/gui/gui.gd
+++ proyecto/escenas/gui/gui.gd
@@ -4,16 +4,12 @@ extends MarginContainer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var max_hp = 1.0
-const step = 0.001
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Bar/Gauge.max_value = max_hp
-	$Bar/Gauge.step = step
-	
-	set_process(false)
-	set_physics_process(false)
+	# Actualizar direccion con la direccion dle nodo player FIXME
+	var player_max_health = $"../Characters/Player".max_health
+	$Bar/Gauge.max_value = player_max_health
 	pass # Replace with function body.
 
 
@@ -21,8 +17,3 @@ func _ready():
 #func _process(delta):
 #	pass
 
-#Set current value of bar
-func set_bar_value(v):
-	var newv = v / max_hp
-	$Bar/Gauge.value = newv
-	$Bar/BackGround/Number.text = str(int(100 * newv))
