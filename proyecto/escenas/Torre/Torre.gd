bd091ebda417ee8eef10540bd1052a9c87d7e5c7
Mapachana <anabuenrua99@gmail.com>
2020-02-01 12:29:02 +0100

    Añado timer a la torre

--- proyecto/escenas/Torre/Torre.gd
+++ proyecto/escenas/Torre/Torre.gd
@@ -3,6 +3,7 @@ extends Area2D
 # puntos de vida actuales y maximos
 var life_points = 0
 var max_life = 100
+var tiempo_espera = 1
 
 # señal que indica cuando se cambia la vida de la torre
 signal tower_health_change
@@ -31,3 +32,11 @@ func _process(delta):
 	if (tiempo > 3):
 		emit_signal("tower_health_change", 50)
 	pass
+
+
+func _on_Torre_body_entered(body):
+	if (body.is_repairing and $Timer.is_stopped()):
+		$Timer.start(tiempo_espera)
+	else:
+		$Timer.stop()
+	pass # Replace with function body.
