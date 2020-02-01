20e5ae35bf97fdf5a8272266e29550daad5d84a6
jojelupipa <jesus_38b@hotmail.com>
2020-02-01 21:47:08 +0100

    Añadida animación en la derrota

---
+++ proyecto/scripts/duck/duck.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+
+# Duck movement vector
+var velocity = Vector2(1,0)
+# Duck direction
+export var direction = 1
+# Duck speed
+export var speed = 15
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if direction == -1:
+		$Sprite.set_flip_h(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var collision = move_and_collide(direction * velocity * speed * delta)
+	if collision:
+		direction *= -1
+		$Sprite.set_flip_h(!$Sprite.flip_h)
+		speed *= 1.5
+
